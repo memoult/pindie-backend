5dd6cdefff02080b0a953150f17b7777dcc9069f
Legion13 <59478217+memoult@users.noreply.github.com>
2024-05-19 20:09:16 +0300

    edit MongoDB

---
+++ middlewars/categories.js
@@ -0,0 +1,8 @@
+const category = require('../models/category');
+
+const findAllCategories = async (req, res, next) => {
+    req.categoriesArray = await category.find({});
+    next();
+}
+
+module.exports = findAllCategories;
---
+++ middlewars/users.js
@@ -0,0 +1,8 @@
+const user = require('../models/user');
+
+const findAllUsers = async (req, res, next) => {
+    req.usersArray = await user.find({});
+    next();
+}
+
+module.exports = findAllUsers;
