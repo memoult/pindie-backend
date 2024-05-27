7c6e489c4cedbec8d00fed7bcfb1868c90175e92
Legion13 <59478217+memoult@users.noreply.github.com>
2024-05-27 20:49:55 +0300

    123

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,20 +3,18 @@ const allowedCors = [
     // 'https://students-projects.ru',
     "http://localhost:3000",
     "http://localhost:3001",
+    "xodorkovskyfrontend.nomorepartiesco.ru",
   ];
   
-  // Функция, которая принимает объекты req (информация о запросе),
-  // res (объект ответа) и функцию next (для запуска следующего миддлвара)
   function cors(req, res, next) {
     const { origin } = req.headers;
+  
     if (allowedCors.includes(origin)) {
-      // Если это наш друг,
-      res.header("Access-Control-Allow-Origin", origin); // говорим: «Добро пожаловать!»
-      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-      res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+      res.header("Access-Control-Allow-Origin", origin);
     }
- 
-    next(); // Идём дальше, не задерживаем очередь
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    next();
   }
   
-  module.exports = cors;
+  module.exports = cors; 
