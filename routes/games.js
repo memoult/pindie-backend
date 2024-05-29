fc99b8133d5d4b6df6f2aa66419176ea19a45173
Legion13 <59478217+memoult@users.noreply.github.com>
2024-05-29 12:15:32 +0300

    123

--- routes/games.js
+++ routes/games.js
@@ -4,24 +4,11 @@ const { checkAuth } = require('../middlewares/auth');
 
 const gamesRouter = require('express').Router();
 
-gamesRouter.post("/games", findAllGames, 
-checkIsGameExists, 
-checkIfCategoriesAvaliable, 
-checkEmptyFields, 
-checkAuth,
-createGame, 
-sendGameCreated);
+gamesRouter.post("/games", findAllGames, checkIsGameExists, checkIfCategoriesAvaliable, checkEmptyFields, checkAuth,createGame, sendGameCreated);
 
 gamesRouter.get('/games', findAllGames, sendAllGames);
 gamesRouter.get("/games/:id", findGameById, sendGameById);
-gamesRouter.put('/games/:id', findGameById, 
-checkIsVoteRequest, 
-checkIfUsersAreSafe, 
-checkIfCategoriesAvaliable, 
-checkEmptyFields, 
-checkAuth, 
-updateGame, 
-sendGameUpdated);
+gamesRouter.put('/games/:id', findGameById, checkIsVoteRequest, checkIfUsersAreSafe, checkIfCategoriesAvaliable, checkEmptyFields, checkAuth, updateGame, sendGameUpdated);
 
 gamesRouter.delete('/games/:id', checkAuth, deleteGame, sendGameDeleted);
 
